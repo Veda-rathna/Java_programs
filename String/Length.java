eba6aee2f2776908224953f4bd037c6776593603
Veda-rathna <gamingguru245@gmail.com>
2024-03-21 14:01:24 +0530

    String Operations

---
+++ String/Length.java
@@ -0,0 +1,8 @@
+package String;
+
+public class Length {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        System.out.println("The length of the string is " + str.length());
+    }
+}
