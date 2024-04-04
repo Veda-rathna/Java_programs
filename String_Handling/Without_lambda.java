65433f8e2b498ac8b213c8c1e5935a64f24cfe37
Veda-rathna <gamingguru245@gmail.com>
2024-04-04 21:03:30 +0530

    Changed interface name

--- String_Handling/Without_lambda.java
+++ String_Handling/Without_lambda.java
@@ -1,13 +1,13 @@
 package String_Handling;
 
-interface Drawable{
+interface Drawable1{
     public void draw();
 }
 
 class Without_lambda{
     public static void main(String[] args) {
         int x = 10;
-        Drawable d1 = new Drawable() {
+        Drawable1 d1 = new Drawable1() {
             public void draw(){
                 System.out.println("The width is " + x);
             }
